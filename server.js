94d8b51cdb483755c4697f75d2a6f4ed7003115b
Mark Ciubal <mark.ciubal@gmail.com>
2026-03-26 16:09:43 -0700

    Working web

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs   = require('fs');
 const WebSocket = require('ws');
 
-const PORT       = 3000;
+const PORT = process.env.PORT || 3000;
 const R          = 1;          // sphere radius (world units)
 const MOVE_SPD   = 0.025;      // radians/tick
 const TURN_SPD   = 0.06;
